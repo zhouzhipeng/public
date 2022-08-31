1830d4a27c2195002f45191e4da84ad35bdc7023
周志鹏 <823143047@qq.com>
2022-08-31 14:45:21 +0800

    Update install_gogo.sh

--- install_gogo.sh
+++ install_gogo.sh
@@ -12,26 +12,8 @@ curl -sSL https://raw.githubusercontent.com/zhouzhipeng/public/main/gogo_bin/web
 chmod +x gogo web
 
 # register service
-cat > /etc/systemd/system/gogo.service  << EOF
-[Unit]
-Description=GoGo Service
-After=network.target
+curl -sSL https://raw.githubusercontent.com/zhouzhipeng/public/main/gogo.service --output /etc/systemd/system/gogo.service
 
-[Service]
-Environment="MAIN_PORT=80"
-Environment="HTTPS_PORT=443"
-Environment="IN_DOCKER=true"
-Environment="BASE_DIR=/tmp"
-Environment="TROJAN_PASSWORD=123456"
-Environment="AUTO_REDIRECT_TO_HTTPS=true"
-Environment="WHITELIST_ROOT_DOMAINS=zhouzhipeng.com"
-Type=simple
-Restart=always
-ExecStart=/root/gogo
-
-[Install]
-WantedBy=multi-user.target
-EOF
 
 systemctl daemon-reload
 
