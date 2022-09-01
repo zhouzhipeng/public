3fbc3201b19c60ab5b9a3eee3062bfb8ba01796d
周志鹏 <823143047@qq.com>
2022-09-01 08:52:17 +0800

    Update install_gogo.sh

--- install_gogo.sh
+++ install_gogo.sh
@@ -6,14 +6,13 @@ set -eux
 
 cd /root
 
-curl -sSL https://raw.githubusercontent.com/zhouzhipeng/public/main/gogo_bin/gogo --output gogo
-curl -sSL https://raw.githubusercontent.com/zhouzhipeng/public/main/gogo_bin/web --output web
+curl -sSL https://github.com/zhouzhipeng/public/releases/download/1.0/gogo_debian.zip --output gogo_debian.zip
+unzip gogo_debian.zip
 
 chmod +x gogo web
 
 # register service
 curl -sSL https://raw.githubusercontent.com/zhouzhipeng/public/main/gogo.service --output /etc/systemd/system/gogo.service
-
 
 systemctl daemon-reload
 
